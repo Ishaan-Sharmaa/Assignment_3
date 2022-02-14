5bac1dd4867583b2bac6ba3d419dbbdc370150a3
Ishaan-Sharmaa <97667648+Ishaan-Sharmaa@users.noreply.github.com>
2022-02-14 23:40:27 +0530

    Add files via upload

---
+++ asg_3_output_cse.py
@@ -0,0 +1,72 @@
+QUESTION 1
+ENTER ANY STRING: HELLO
+{'H': 1, 'E': 1, 'L': 2, 'O':1}
+
+
+QUESTION 2
+ENTER THE DAY: 12
+ENTER THE MONTH OF THE YEAR: 02
+ENTER THE YEAR: 2003
+13 / 2 / 2003
+
+
+QUESTION 3
+Enter elements of a list separated by space 1 2 3
+list:  1 4 9
+[(1, 1), (2, 4), (3, 9)]
+
+
+QUESTION 4
+Enter Grade Point: 5
+Your Grade is 'C' and Below Average performance
+
+
+QUESTION 5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+QUESTION 6
+Enter SID: 21103107
+Enter Name: NAMAN
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103108
+Enter Name: ARTH
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103109
+Enter Name: SHREY
+Do you want to enter another student entry(Y or N):Y
+Enter SID: 21103110
+Enter Name: DHRUV
+Do you want to enter another student entry(Y or N):N
+<a> {21103107: 'NAMAN', 21103108: 'ARTH', 21103109: 'SHREY', 21103110: 'DHRUV'}
+<b> {21103110: 'DHRUV', 21103107: 'NAMAN', 21103108: 'ARTH', 21103109: 'SHREY'}
+<c> {21103107: 'NAMAN', 21103108: 'ARTH', 21103109: 'SHREY', 21103110: 'DHRUV'}
+Search Name with SID: 21103108
+<d> PARTH
+
+
+
+QUESTION 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 6
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 2
+
+
+
+QUESTION 8
+<a> {1, 3, 5, 6, 8}
+<b> {3, 6, 8, 9, 13, 17}
+<c> {1, 2, 4, 5}
+<d> {6, 7, 8, 9, 10}
+<e> {10, 7}
